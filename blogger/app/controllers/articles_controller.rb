f8e063cf120c65ff904262bb83ed77f2c5feb43b
Rene <cheesus928@gmail.com>
2016-06-11 11:43:20 -0500

    Finished comments

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -6,10 +6,13 @@ class ArticlesController < ApplicationController
     
     def show
         @article = Article.find(params[:id])
+        @comment = Comment.new
+        @comment.article_id = @article.id
     end
     
     def new
         @article = Article.new
+
     end
     
     def create
